ef9a87ab44de973ed9f21dc0736330f4c7d8106c
Niranjan V <110783514+code-with-niranjan-v@users.noreply.github.com>
2023-05-15 23:06:27 +0530

    Update List

--- src/main/kotlin/13.List.kt
+++ src/main/kotlin/13.List.kt
@@ -1,6 +1,6 @@
 fun main(){
 	
-	
+	//List is a Ordered Collections of objects.
 	var numbers = listOf(1,23,4,5)
 	
 	//Iterating
@@ -12,5 +12,10 @@ fun main(){
 	//Contains and ContainsAll
 	println(numbers.contains(1))
 	println(numbers.containsAll(listOf(23,7)))
+
+	//List are immutable by Default.
+	//So, we cannot modify them.
+
+	
 
 }
