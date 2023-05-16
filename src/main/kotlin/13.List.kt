e99ec6a845b64b89be6c6884990a67a6f54565b1
Niranjan <rathinamniranjan@gmail.com>
2023-05-16 19:12:03 +0530

    List Update

--- src/main/kotlin/13.List.kt
+++ src/main/kotlin/13.List.kt
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //List
 
 //List is an ordered collection of objects
@@ -44,34 +43,3 @@ fun main() {
     //Creating a mutable list
 
 }
-=======
-fun main(){
-	
-	//List is a Ordered Collections of objects.
-	var numbers = listOf(1,23,4,5)
-	
-	//Iterating
-	//Method 1
-	numbers.forEach{
-		println(it)
-}
-	//Method 2
-	for(i in numbers){
-		println(i)
-	}
-
-	//Indexing
-	println(numbers[3])
-
-
-	//Contains and ContainsAll
-	println(numbers.contains(1))
-	println(numbers.containsAll(listOf(23,7)))
-
-	//List are immutable by Default.
-	//So, we cannot modify them.
-
-	
-
-}
->>>>>>> origin/main
