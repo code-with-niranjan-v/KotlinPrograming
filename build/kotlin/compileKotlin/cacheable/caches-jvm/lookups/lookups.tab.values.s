ec2fe285281d4fc3a986544ce66d1dc569134e69
Niranjan <rathinamniranjan@gmail.com>
2023-05-20 23:50:54 +0530

    Setters and Getters 2

--- build/kotlin/compileKotlin/cacheable/caches-jvm/lookups/lookups.tab.values.s
+++ build/kotlin/compileKotlin/cacheable/caches-jvm/lookups/lookups.tab.values.s
@@ -1,1 +1,1 @@
-ÅlÕgÑdÑhÜdÏgýgähïhìaõbñ_È]×YûVñWïVüYÆRêTÜ[í]óYÀYØZÇZýYã5û{ãXçXÎZËbêcÈdîcÐ:ÂŠÒbêVÇVôQÕMâOÄOØMû?ÂtíxôDÙ®ìµöµëµÇ¶Ý´ñNå‘ÞB
+ÅlÕgÑdÑhÜdÏgýgähïhìaõbñ_È]×YûVñWïVüYÆRêTÜ[í]óYÀYØZÇZýYã5û{ãXçXÎZËbêcÈdîcÐ:ÂŠÒbêVÇVôQÕMâOÄOØMû?ÂtíxôDÙ®ìµöµëµÇ¶Ý´ñNå‘ÞBÿZ
--- src/main/kotlin/20.SetterandGetter.kt
+++ src/main/kotlin/20.SetterandGetter.kt
@@ -22,7 +22,14 @@ class Check{
     var age = 20
 
     var result = "None"
-        get() = field
+        get() {
+
+            if(age>18){
+                return "Eligible"
+            }else{
+                return "Not Eligible"
+            }
+        }
         set(value){
 
             if(age>18){
@@ -40,7 +47,23 @@ fun main(){
     val check = Check()
 
     check.result = "Check Eligible"//It will run set block of code
-    //and check condition if age is > 18 and set it value.
+    //and check condition if age is > 18 and set its value.
+    println(check.result)
+    //From the above satement We are accessing the result
+    //So, it will run get statement.
+
+    /*
+
+        The output is eligible because when we assign some to result
+        variable it runs set block, and it checks the condition
+        if age is greater than 20 and the age is 20 which
+        greater than 18, it prints eligible.
+
+     */
+
+    //Now let us change age value
+
+    check.age = 17
     println(check.result)
 
 }
