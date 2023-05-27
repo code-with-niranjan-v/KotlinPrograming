5181877cd84b7494bc4e23f4e4d537f3e6597deb
Niranjan <rathinamniranjan@gmail.com>
2023-05-28 01:30:51 +0530

    Generics

--- src/main/kotlin/28.Generics.kt
+++ src/main/kotlin/28.Generics.kt
@@ -19,9 +19,34 @@ class Details<T>(var element1:T,var element2:T){
 
 }
 
+class Search<T>(var elements:ArrayList<T>,var target:T){
+
+    fun getElementIndex(){
+
+        for (element in elements){
+
+            if(element == target){
+
+                println("$target is found at ${elements.indexOf(element)}")
+                break
+
+            }
+
+
+        } 
+
+
+    }
+
+}
+
 
 fun main() {
 
     val j1 = Details(12,13)
     val j2 = Details("Hello ","World!")
+    val s1 = Search(arrayListOf(1,5,4,8,9,2,3,4),3)
+    val s2 = Search(arrayListOf("A","B","C","Z","D","S"),"Z")
+    s1.getElementIndex()
+    s2.getElementIndex()
 }
